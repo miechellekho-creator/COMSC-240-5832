087c1ebb801c3df7598567b2cb166401dc7d7315
miechelle Kho <miechellekho@gmail.com>
2026-06-20 22:30:09 -0700

    fix syntax error

---
+++ main.py
@@ -0,0 +1,10 @@
+nums = [3, 8, 1, 9, 4, 7, 2, 6, 5, 0]
+n = len(nums)
+
+for i in range(n):
+    temp = nums[i]
+    nums[i] = nums[n - 1 - i]
+    nums[n - 1 - i] = temp
+
+print(nums)
+return nums
