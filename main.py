e0b4afe0462b6807bff10bba35a9e37741411f3e
miechelle Kho <miechellekho@gmail.com>
2026-06-20 22:44:27 -0700

    fix loop count

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 nums = [3, 8, 1, 9, 4, 7, 2, 6, 5, 0]
 n = len(nums)
 
-for i in range(n):
+for i in range(n // 2):
     temp = nums[i]
     nums[i] = nums[n - 1 - i]
     nums[n - 1 - i] = temp
 
 print(nums)
-return nums
